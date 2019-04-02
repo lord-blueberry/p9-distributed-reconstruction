e9c7f9410b30e7c02e1ac82241c732ee5bfe9d02
Jonas Schwammberger <jonas.schwammberger@fhnw.ch>
2019-04-02 11:21:34 +0200

    Added first CDCLEAN implementation

---
+++ Distributed-Reconstruction/Single-Machine/Deconvolution/CDClean.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Single_Machine.Deconvolution
+{
+    /// <summary>
+    /// Cooordinate Descent "CLEAN" iteration.
+    /// 
+    /// It uses Coordinate Descent to minimize the deconvolution of a single pixel.
+    /// </summary>
+    public class CDClean
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="xImage">resulting deconvolved image. If this is the first Major cycle, xImage[i] = 0</param>
+        /// <param name="residual">residual image</param>
+        /// <param name="psf">point spread function in image space</param>
+        /// <param name="lambda">regularization parameter</param>
+        public static void CoordinateDescent(double[,] xImage, double[,] residual, double[,] psf, double lambda)
+        {
+            // the "a" of the parabola equation a* x*x + b*x + c
+            var a = CalcPSFSquared(psf);
+
+            for (int y = 0; y < residual.GetLength(0); y++)
+            {
+                for(int x = 0; x < residual.GetLength(1); x++)
+                {
+                    var xOld = xImage[y, x];
+                    var b = CalculateB(residual, psf, y, x);
+
+                    //calculate minimum of parabola, eg -2b/a
+                    var xNew = xOld + (b / a);
+                    xNew = ShrinkAbsolute(xNew, lambda);
+
+                    if(xNew != xOld)
+                    {
+                        xImage[y, x] = xNew;
+                        var xDiff =  xOld - xNew;
+                        ModifyResidual(residual, psf, y, x, xDiff);
+                    }
+                }
+            }
+        }
+
+        private static double CalculateB(double[,] residual, double[,] psf, int y, int x)
+        {
+            int yOffset = y - psf.GetLength(0) / 2;
+            int xOffset = x - psf.GetLength(1) / 2;
+
+            var b = 0.0;
+            for (int i = 0; i < psf.GetLength(0); i++)
+            {
+                for (int j = 0; j < psf.GetLength(1); j++)
+                {
+                    var ySrc = yOffset + i;
+                    var xSrc = xOffset + j;
+                    if (ySrc >= 0 & ySrc < residual.GetLength(0) &
+                        xSrc >= 0 & xSrc < residual.GetLength(1))
+                    {
+                        b += residual[ySrc, xSrc] * psf[i, j];
+                    }
+                }
+            }
+
+            return b;
+        }
+
+        private static void ModifyResidual(double[,] residual, double[,] psf, int y, int x, double xDiff)
+        {
+            int yOffset = y - psf.GetLength(0) / 2;
+            int xOffset = x - psf.GetLength(1) / 2;
+            for (int i = 0; i < psf.GetLength(0); i++)
+            {
+                for (int j = 0; j < psf.GetLength(1); j++)
+                {
+                    var yDst = yOffset + i;
+                    var xDst = xOffset + j;
+                    if (yDst >= 0 & yDst < residual.GetLength(0) &
+                        xDst >= 0 & xDst < residual.GetLength(1))
+                    {
+                        residual[yDst, xDst] = xDiff * psf[i, j];
+                    }
+                }
+            }
+        }
+
+        private static double CalcPSFSquared(double[,] psf)
+        {
+            double squared = 0;
+            for (int y = 0; y < psf.GetLength(0); y++)
+                for (int x = 0; x < psf.GetLength(0); x++)
+                    squared += psf[y, x] * psf[y, x];
+            
+            return squared;
+        }
+
+        private static double ShrinkAbsolute(double value, double lambda) 
+        {
+            value = Math.Abs(value) - lambda;
+            return Math.Max(value, 0.0);
+        }
+
+
+    }
+}
