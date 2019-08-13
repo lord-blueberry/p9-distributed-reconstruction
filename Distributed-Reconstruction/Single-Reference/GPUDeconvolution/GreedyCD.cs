c3c634c36db5993de59c9a8e82ba5c7ca950b9f7
Jonas Schwammberger <jonas.schwammberger@fhnw.ch>
2019-08-13 11:40:44 +0200

    maybe a working iteration of CD in ILGPU

--- Distributed-Reconstruction/Single-Reference/GPUDeconvolution/GreedyCD.cs
+++ Distributed-Reconstruction/Single-Reference/GPUDeconvolution/GreedyCD.cs
@@ -4,7 +4,6 @@ using System.Text;
 
 using ILGPU;
 using ILGPU.Lightning;
-using ILGPU.Lightning.Sequencers;
 using ILGPU.ReductionOperations;
 using ILGPU.Runtime;
 using ILGPU.Runtime.CPU;
@@ -15,9 +14,87 @@ namespace Single_Reference.GPUDeconvolution
 {
     public class GreedyCD
     {
+        private static float GPUShrinkElasticNet(float value, float lambda, float alpha) => GPUMath.Max(value - lambda * alpha, 0.0f) / (1 + lambda * (1 - alpha));
+
+        #region kernels
+        private static void ShrinkKernel(Index2 index,
+                                         ArrayView2D<float> xImage,
+                                         ArrayView2D<float> xCandidates,
+                                         ArrayView2D<float> shrinked,
+                                         ArrayView<float> lambdaAlpha)
+        {
+            if(index.InBounds(shrinked.Extent))
+            {
+                var xOld = xImage[index];
+                var xCandidate = xCandidates[index];
+                var lambda = lambdaAlpha[0];
+                var alpha = lambdaAlpha[1];
+
+                var xNew = GPUShrinkElasticNet(xOld + xCandidate, lambda, alpha);
+                shrinked[index] = GPUMath.Abs(xOld - xNew);
+            }
+        }
+
+        private static void MaxIndexKernel(Index2 index,
+                                       ArrayView2D<float> xImage,
+                                       ArrayView2D<float> xCandidate,
+                                       ArrayView2D<float> shrinked,
+                                       ArrayView<float> maxAbsDiff,
+                                       ArrayView<int> maxIndices,
+                                       ArrayView<float> lambdaAlpha)
+        {
+            //not sure if necessary, but bounds check were always done in the ILGPU examples
+            if (index.InBounds(xImage.Extent))
+            {
+                //TODO: fix this line for ximage.size != xCandidates.size
+                var shrink = shrinked[index];
+                var max = maxAbsDiff[0];
+
+                if (shrink == max)
+                {
+                    var oldValue = Atomic.CompareExchange(maxIndices.GetVariableView(0), -1, index.Y);
+                    if (oldValue == -1)
+                    {
+                        maxIndices[1] = index.X;
+
+                        //retrieve sign of maximum candidate
+                        var lambda = lambdaAlpha[0];
+                        var alpha = lambdaAlpha[1];
+                        var xNew = GPUShrinkElasticNet(xImage[index] + xCandidate[index], lambda, alpha);
+
+                        //update result
+                        xImage[index] = xNew;
+                        maxAbsDiff[0] = xImage[index] - xNew;
+                    }
+                }
+            }
+        }
+
+        private static void UpdateCandidatesKernel(Index2 index,
+                                                   ArrayView2D<float> xCandidates,
+                                                   ArrayView2D<float> aMap,
+                                                   ArrayView2D<float> psf2,
+                                                   ArrayView<float> maxDiff,
+                                                   ArrayView<int> maxIndices)
+        {
+            var indexCandidate = index.Add(new Index2(maxIndices[1], maxIndices[0])).Subtract(psf2.Extent / 2);
+            if (index.InBounds(psf2.Extent) & indexCandidate.InBounds(xCandidates.Extent))
+            {
+                xCandidates[indexCandidate] += (psf2[index] * maxDiff[0]) / aMap[index];
+            }
+        }
+
+        private static void ResetIndicesKernel(Index index,
+                                               ArrayView<int> maxIndices)
+        {
+            maxIndices[index] = -1;
+        }
+        #endregion
+
         private static void Iteration(Accelerator accelerator)
         {
-            var maxIndexKernel = accelerator.LoadAutoGroupedStreamKernel<Index2, ArrayView2D<float>, ArrayView2D<float>, ArrayView<float>, ArrayView<int>>(MaxIndexKernel);
+            var shrinkKernel = accelerator.LoadAutoGroupedStreamKernel<Index2, ArrayView2D<float>, ArrayView2D<float>, ArrayView2D<float>, ArrayView<float>>(ShrinkKernel);
+            var maxIndexKernel = accelerator.LoadAutoGroupedStreamKernel<Index2, ArrayView2D<float>, ArrayView2D<float>, ArrayView2D<float>, ArrayView<float>, ArrayView<int>, ArrayView<float>>(MaxIndexKernel);
             var updateCandidatesKernel = accelerator.LoadAutoGroupedStreamKernel<Index2, ArrayView2D<float>, ArrayView2D<float>, ArrayView2D<float>, ArrayView<float>, ArrayView<int>>(UpdateCandidatesKernel);
             var resetKernel = accelerator.LoadAutoGroupedStreamKernel<Index, ArrayView<int>>(ResetIndicesKernel);
 
@@ -26,10 +103,12 @@ namespace Single_Reference.GPUDeconvolution
 
             using (var xImage = accelerator.Allocate <float>(size))
             using (var xCandidates = accelerator.Allocate<float>(size))
+            using (var shrinked = accelerator.Allocate<float>(size))
             using (var aMap = accelerator.Allocate<float>(size))
             using (var psf2 = accelerator.Allocate<float>(psfSize))
             using (var maxCandidate = accelerator.Allocate<float>(1))
             using (var maxIndices = accelerator.Allocate<int>(2))
+            using (var lambdaAlpha = accelerator.Allocate<float>(2))
             {
                 xImage.MemSetToZero();
                 xCandidates.MemSetToZero();
@@ -44,13 +123,20 @@ namespace Single_Reference.GPUDeconvolution
                 maxIndices[0] = -1;
                 maxIndices[1] = -1;
 
+                lambdaAlpha[0] = 0.1f;
+                lambdaAlpha[1] = 1.0f;
+
+                shrinkKernel(size, xImage.View, xCandidates.View, shrinked.View, lambdaAlpha.View);
+
                 if (accelerator.AcceleratorType == AcceleratorType.CPU)
-                    accelerator.Reduce(xCandidates.View.AsLinearView(), maxCandidate.View, new ShuffleDownFloat(), new AtomicMaxFloat());
+                    accelerator.Reduce(shrinked.View.AsLinearView(), maxCandidate.View, new ShuffleDownFloat(), new AtomicMaxFloat());
                 else
-                    accelerator.Reduce(xCandidates.View.AsLinearView(), maxCandidate.View, new ShuffleDownFloat(), new MaxFloat());
+                    accelerator.Reduce(shrinked.View.AsLinearView(), maxCandidate.View, new ShuffleDownFloat(), new MaxFloat());
                 accelerator.Synchronize();
 
-                maxIndexKernel(size, xImage.View, xCandidates.View, maxCandidate.View, maxIndices.View);
+                //up to here is good
+
+                maxIndexKernel(size, xImage.View, xCandidates.View, shrinked.View, maxCandidate.View, maxIndices.View, lambdaAlpha.View);
                 accelerator.Synchronize();
 
                 updateCandidatesKernel(psfSize, xCandidates.View, aMap.View, psf2.View, maxCandidate.View, maxIndices.View);
@@ -61,126 +147,14 @@ namespace Single_Reference.GPUDeconvolution
 
                 var x = xImage.GetAsArray();
                 var candidate = xCandidates.GetAsArray();
+                var p = psf2.GetAsArray();
                 var maxI = maxIndices.GetAsArray();
             }
         }
         
-        private static void MaxIndexKernel(Index2 index,
-                                           ArrayView2D<float> xImage,
-                                           ArrayView2D<float> xCandidates,
-                                           ArrayView<float> maxCandidate,
-                                           ArrayView<int> maxIndices)
-        {
-            //not sure if necessary, but bounds check were always done in the ILGPU examples
-            if(index.InBounds(xImage.Extent))
-            {
-                //TODO: fix this line for ximage.size != xCandidates.size
-                var myCandidate = xCandidates[index];
-                var max = maxCandidate[0];
-
-                if(myCandidate == max)
-                {
-                    var oldValue = Atomic.CompareExchange(maxIndices.GetVariableView(0), -1, index.Y);
-                    if (oldValue == -1)
-                    {
-                        maxIndices[1] = index.X;
-
-                        //update result
-                        xImage[index] -= max;
-                    }   
-                }
-            }
-        }
 
 
-        private static void UpdateCandidatesKernel(Index2 index,
-                                                   ArrayView2D<float> xCandidates,
-                                                   ArrayView2D<float> aMap,
-                                                   ArrayView2D<float> psf2,
-                                                   ArrayView<float> maxCandidate,
-                                                   ArrayView<int> maxIndices)
-        {
-            var indexCandidate = index.Add(new Index2(maxIndices[1], maxIndices[0])).Subtract(psf2.Extent /2);
-            if (index.InBounds(psf2.Extent) & indexCandidate.InBounds(xCandidates.Extent))
-            {
-                xCandidates[indexCandidate] -= psf2[index];
-            }
-        }
 
-        private static void ResetIndicesKernel(Index index,
-                                               ArrayView<int> maxIndices)
-        {
-            maxIndices[index] = -1;
-        }
-
-
-        /// <summary>
-        /// Demonstrates the reduce functionality.
-        /// </summary>
-        /// <param name="accl">The target accelerator.</param>
-        static void Reduce(Accelerator accl)
-        {
-            var index = new Index2(32, 32);
-            using (var buffer = accl.Allocate<int>(index))
-            {
-                for (int i = 0; i < 32; i++)
-                    for (int j = 0; j < 32; j++)
-                        buffer[new Index2(i, j)] = i + j;
-
-                using (var target = accl.Allocate<int>(1))
-                {
-                    
-                    // This overload requires an explicit output buffer but
-                    // uses an implicit temporary cache from the associated accelerator.
-                    // Call a different overload to use a user-defined memory cache.
-                    accl.Reduce(
-                        buffer.View.AsLinearView(),
-                        target.View,
-                        new ShuffleDownInt32(),
-                        new MaxInt32());
-
-                    accl.Synchronize();
-
-                    var data = target.GetAsArray();
-                    for (int i = 0, e = data.Length; i < e; ++i)
-                        Console.WriteLine($"Reduced[{i}] = {data[i]}");
-                }
-            }
-        }
-
-        /// <summary>
-        /// Demonstrates the reduce functionality.
-        /// </summary>
-        /// <param name="accl">The target accelerator.</param>
-        static void AtomicReduce(Accelerator accl)
-        {
-            var index = new Index2(32, 32);
-            using (var buffer = accl.Allocate<int>(index))
-            {
-
-                for (int i = 0; i < 32; i++)
-                    for (int j = 0; j < 32; j++)
-                        buffer[new Index2(i, j)] = i + j;
-                using (var target = accl.Allocate<int>(1))
-                {
-
-                    // This overload requires an explicit output buffer but
-                    // uses an implicit temporary cache from the associated accelerator.
-                    // Call a different overload to use a user-defined memory cache.
-                    accl.AtomicReduce(
-                        buffer.View.AsLinearView(),
-                        target.View,
-                        new ShuffleDownInt32(),
-                        new AtomicMaxInt32());
-
-                    accl.Synchronize();
-
-                    var data = target.GetAsArray();
-                    for (int i = 0, e = data.Length; i < e; ++i)
-                        Console.WriteLine($"AtomicReduced[{i}] = {data[i]}");
-                }
-            }
-        }
 
         public static void Test()
         {
