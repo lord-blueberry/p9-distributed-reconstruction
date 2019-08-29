4186ca342cd66ee6de077d99afab797a14aca77d
Jonas Schwammberger <jonas.schwammberger@fhnw.ch>
2019-08-29 11:53:11 +0200

    working with four?

--- Distributed-Reconstruction/Single-Reference/Deconvolution/RandomBlockCD.cs
+++ Distributed-Reconstruction/Single-Reference/Deconvolution/RandomBlockCD.cs
@@ -19,50 +19,19 @@ namespace Single_Reference.Deconvolution
                     psf[i, j] = 0.5;
             psf[16, 16] = 1.0;
 
-            /*
-            var A = new DenseMatrix(4, 4);
-            var corr = new double[3];
-            corr[0] = 1/CalcAMatrixEntry(psf, 0, 1);
-            corr[1] = 1/CalcAMatrixEntry(psf, 0, 2);
-            corr[2] = 1/CalcAMatrixEntry(psf, 0, 3);
-            var sum = 0.0;
-            InfToZero(corr);
-            for (int i = 0; i < corr.Length; i++)
-                sum += corr[i];
+   
+            var a = new DenseVector(4);
+            a[0] = CalcAMatrixEntry(psf, 0, 0);
+            a[1] = CalcAMatrixEntry(psf, 0, 1);
+            a[2] = CalcAMatrixEntry(psf, 0, 2);
+            a[3] = CalcAMatrixEntry(psf, 0, 3);
+            /*a[4] = CalcAMatrixEntry(psf, 0, 2);
+            a[5] = CalcAMatrixEntry(psf, 2, 0);
+            a[6] = CalcAMatrixEntry(psf, 1, 2);
+            a[7] = CalcAMatrixEntry(psf, 2, 1);
+            a[8] = CalcAMatrixEntry(psf, 2, 2);*/
 
-            A[0, 0] =  CalcAMatrixEntry(psf, 0, 0);
-            A[1, 1] = A[0, 0];
-            A[2, 2] = A[0, 0];
-            A[1, 0] = CalcAMatrixEntry(psf, 0, 1);
-            A[0, 1] = A[1, 0];
-            A[1, 2] = A[1, 0];
-            A[2, 1] = A[1, 0];
-            A[2, 3] = A[1, 0];
-            A[3, 2] = A[1, 0];
-            A[2, 0] = CalcAMatrixEntry(psf, 0, 2);
-            A[0, 2] = A[2, 0];
-            A[3, 1] = A[2, 0];
-            A[1, 3] = A[2, 0];
-            A[3, 0] = CalcAMatrixEntry(psf, 0, 3);
-            A[0, 3] = A[3, 0];*/
-
-
-            var A = new DenseMatrix(3, 3);
-            /*A[0, 0] = CalcAMatrixEntry(psf, 0, 0);
-            A[1, 1] = A[0, 0];
-            A[1, 0] = CalcAMatrixEntry(psf, 0, 1);
-            A[0, 1] = A[1, 0];*/
-
-            A[0, 0] = CalcAMatrixEntry(psf, 0, 0);
-            A[1, 1] = A[0, 0];
-            A[2, 2] = A[0, 0];
-            A[1, 0] = CalcAMatrixEntry(psf, 0, 1);
-            A[0, 1] = A[1, 0];
-            A[1, 2] = A[1, 0];
-            A[2, 1] = A[1, 0];
-            A[2, 0] = CalcAMatrixEntry(psf, 0, 2);
-            A[0, 2] = A[2, 0];
-
+            var A = CreateA(a);
             var blaA = A.ToArray();
             var inv = A.Inverse();
             var arr = inv.ToArray();
@@ -70,10 +39,10 @@ namespace Single_Reference.Deconvolution
             var xImage = new double[32, 32];
             var dirty = new double[32, 32];
 
-            dirty[16, 16] = 1.0;
+            //dirty[16, 16] = 1.0;
             //dirty[16, 17] = 1.8;
-            dirty[16, 18] = 1.1;
-            //dirty[16, 19] = 0.5;
+            //dirty[16, 18] = 1.1;
+            dirty[16, 19] = 0.5;
             var IMG = FFT.Forward(dirty, 1.0);
             var PSF = FFT.Forward(psf, 1.0);
             var CONV = Common.Fourier2D.Multiply(IMG, PSF);
@@ -86,23 +55,29 @@ namespace Single_Reference.Deconvolution
             FFT.Shift(bMap);
 
             FitsIO.Write(residuals, "resConf.fits");
-            var bVec = new DenseVector(3);
+            var bVec = new DenseVector(4);
             bVec[0] = bMap[16, 16];
             bVec[1] = bMap[16, 17];
             bVec[2] = bMap[16, 18];
-            //bVec[3] = bMap[16, 19];
+            bVec[3] = bMap[16, 19];/*
+            bVec[4] = bMap[16, 18];
+            bVec[5] = bMap[18, 16];
+            bVec[6] = bMap[17, 18];
+            bVec[7] = bMap[18, 17];
+            bVec[8] = bMap[18, 18];*/
 
- 
-            var res3 = (inv * bVec).ToArray();
+
+            var res3 = (bVec * inv).ToArray();
             xImage[16, 16] = res3[0];
             xImage[16, 17] = res3[1];
             xImage[16, 18] = res3[2];
-            //xImage[16, 19] = res3[3];
+            xImage[16, 19] = res3[3];
             var XIMG = FFT.Forward(xImage, 1.0);
             var RESCONV = Common.Fourier2D.Multiply(XIMG, PSF);
             var results = FFT.Backward(RESCONV, (double)(BMAP.GetLength(0) * BMAP.GetLength(1)));
             FFT.Shift(results);
             FitsIO.Write(results, "dirtyConf.fits");
+            FitsIO.Write(xImage, "dXXConf.fits");
 
         }
         public static bool Deconvolve(double[,] xImage, double[,] residuals, double[,] psf, double lambda, double alpha, int maxIteration = 100, double epsilon = 1e-4)
@@ -131,6 +106,20 @@ namespace Single_Reference.Deconvolution
                 }
 
             return output;
+        }
+
+        private static DenseMatrix CreateA(DenseVector vec)
+        {
+            var A = new DenseMatrix(vec.Count, vec.Count);
+            for(int i = 0; i < vec.Count; i++)
+            {
+                for(int j = 0; j < vec.Count; j++)
+                {
+                    A[j, i] = vec[Math.Abs((j - i) % vec.Count)];
+                }
+            }
+
+            return A;
         }
 
         private static void InfToZero(double[] mat)
