2ab4bb86ed6c3c5ddb4b0f4dfacf5bb78d9d873c
Jonas Schwammberger <1262464+lord-blueberry@users.noreply.github.com>
2019-09-20 12:52:39 +0200

    creating splitted reading

--- Distributed-Reconstruction/Single-Reference/FitsIO.cs
+++ Distributed-Reconstruction/Single-Reference/FitsIO.cs
@@ -212,6 +212,44 @@ namespace Single_Reference
             return uvw;
         }
 
+        public static double[,,] ReadUVW(string file, int fromBl, int toBl)
+        {
+            var f = new Fits(file);
+            var h = (ImageHDU)f.ReadHDU();
+
+            // Double Cube Dimensions: baseline, time, uvw
+            var uvw_raw = (Array[])h.Kernel;
+            var time_samples = uvw_raw[0].Length;
+            var length = toBl - fromBl;
+
+            var uvw = new double[length, time_samples, 3];
+            for (long i = fromBl; i < toBl; i++)
+            {
+                Array[] bl = (Array[])uvw_raw[i];
+                for (int j = 0; j < time_samples; j++)
+                {
+                    double[] values = (double[])bl[j];
+                    uvw[i - fromBl, j, 0] = values[0]; //u
+                    uvw[i - fromBl, j, 1] = -values[1]; //v
+                    uvw[i - fromBl, j, 2] = values[2]; //w
+                }
+            }
+            return uvw;
+        }
+
+
+        public static int CountBaselines(string file)
+        {
+            var f = new Fits(file);
+            var h = (ImageHDU)f.ReadHDU();
+
+            // Double Cube Dimensions: baseline, time, uvw
+            var uvw_raw = (Array[])h.Kernel;
+            var baselines = uvw_raw.Length;
+
+            return baselines;
+        }
+
         /// <summary>
         /// Read Intensity Visibilities
         /// </summary>
@@ -241,6 +279,37 @@ namespace Single_Reference
 
                         //add polarizations XX and YY together to form Intensity Visibilities only
                         visibilities[i, j, k] = new Complex(
+                            (pol_XX[0] + pol_YY[0]) / norm,
+                            (pol_XX[1] + pol_YY[1]) / norm);
+                    }
+                }
+            }
+            return visibilities;
+        }
+
+        public static Complex[,,] ReadVisibilities(string file, int fromBl, int toBl, int timessamplesCount, int channelsCount, double norm)
+        {
+            var f = new Fits(file);
+            var h = (ImageHDU)f.ReadHDU();
+            //Double cube Dimensions: baseline, time, channel, pol, complex_component
+            var vis_raw = (Array[])h.Kernel;
+            var length = toBl - fromBl;
+
+            var visibilities = new Complex[length, timessamplesCount, channelsCount];
+            for (int i = fromBl; i < toBl; i++)
+            {
+                Array[] bl = (Array[])vis_raw[i];
+                for (int j = 0; j < timessamplesCount; j++)
+                {
+                    Array[] times = (Array[])bl[j];
+                    for (int k = 0; k < channelsCount; k++)
+                    {
+                        Array[] channel = (Array[])times[k];
+                        double[] pol_XX = (double[])channel[0];
+                        double[] pol_YY = (double[])channel[3];
+
+                        //add polarizations XX and YY together to form Intensity Visibilities only
+                        visibilities[i - fromBl, j, k] = new Complex(
                             (pol_XX[0] + pol_YY[0]) / norm,
                             (pol_XX[1] + pol_YY[1]) / norm);
                     }
@@ -280,44 +349,45 @@ namespace Single_Reference
 
             return output;
         }
+
+        public static bool[,,] ReadFlags(string file, int fromBl, int toBl, int timessamplesCount, int channelsCount)
+        {
+            var length = toBl - fromBl;
+            var output = new bool[length, timessamplesCount, channelsCount];
+
+            var f = new Fits(file);
+            var h = (ImageHDU)f.ReadHDU();
+            var flags_raw = (Array[])h.Kernel;
+            for (int i = fromBl; i < toBl; i++)
+            {
+                Array[] bl = (Array[])flags_raw[i];
+                for (int j = 0; j < timessamplesCount; j++)
+                {
+                    Array[] times = (Array[])bl[j];
+                    for (int k = 0; k < channelsCount; k++)
+                    {
+                        double[] pols = (double[])times[k];
+                        var sum = 0.0;
+                        for (int l = 0; l < pols.Length; l++)
+                            sum += pols[l];
+
+                        //visibilitiy has been flagged                        
+                        if (sum == 0)
+                            output[i - fromBl, j, k] = false;
+                        else
+                            output[i - toBl, j, k] = true;
+                    }
+                }
+            }
+
+            return output;
+        }
         #endregion
 
         #region stitching
-        public static double[,,] Stitch(double[,,] x, double[,,] y)
+        public static T[,,] Stitch<T>(T[,,] x, T[,,] y)
         {
-            var output = new double[x.GetLength(0) + y.GetLength(0), x.GetLength(1), x.GetLength(2)];
-            for (int i = 0; i < x.GetLength(0); i++)
-                for (int j = 0; j < x.GetLength(1); j++)
-                    for (int k = 0; k < x.GetLength(2); k++)
-                        output[i, j, k] = x[i, j, k];
-
-            for (int i = 0; i < y.GetLength(0); i++)
-                for (int j = 0; j < x.GetLength(1); j++)
-                    for (int k = 0; k < x.GetLength(2); k++)
-                        output[i + x.GetLength(0), j, k] = y[i, j, k];
-
-            return output;
-        }
-
-        public static bool[,,] Stitch(bool[,,] x, bool[,,] y)
-        {
-            var output = new bool[x.GetLength(0) + y.GetLength(0), x.GetLength(1), x.GetLength(2)];
-            for (int i = 0; i < x.GetLength(0); i++)
-                for (int j = 0; j < x.GetLength(1); j++)
-                    for (int k = 0; k < x.GetLength(2); k++)
-                        output[i, j, k] = x[i, j, k];
-
-            for (int i = 0; i < y.GetLength(0); i++)
-                for (int j = 0; j < x.GetLength(1); j++)
-                    for (int k = 0; k < x.GetLength(2); k++)
-                        output[i + x.GetLength(0), j, k] = y[i, j, k];
-
-            return output;
-        }
-
-        public static Complex[,,] Stitch(Complex[,,] x, Complex[,,] y)
-        {
-            var output = new Complex[x.GetLength(0) + y.GetLength(0), x.GetLength(1), x.GetLength(2)];
+            var output = new T[x.GetLength(0) + y.GetLength(0), x.GetLength(1), x.GetLength(2)];
             for (int i = 0; i < x.GetLength(0); i++)
                 for (int j = 0; j < x.GetLength(1); j++)
                     for (int k = 0; k < x.GetLength(2); k++)
