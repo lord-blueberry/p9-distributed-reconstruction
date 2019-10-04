828eb596fc3b742fabfe4e3343b7c8b9ff3f04d3
Jonas Schwammberger <1262464+lord-blueberry@users.noreply.github.com>
2019-10-04 11:07:34 +0200

    creating active set heuristic for approx

--- Distributed-Reconstruction/Single-Reference/Deconvolution/ToyImplementations/ApproxSingle.cs
+++ Distributed-Reconstruction/Single-Reference/Deconvolution/ToyImplementations/ApproxSingle.cs
@@ -8,37 +8,43 @@ namespace Single_Reference.Deconvolution.ToyImplementations
 {
     class ApproxSingle
     {
+        int yBlockSize;
+        int xBlockSize;
+        int degreeOfSeperability;
+        int tau;
+
         public class ApproxInfo
         {
             public bool CorrectionUsed;
             
         }
 
-        public static bool DeconvolveRandom2(float[,] xImage, float[,] residuals, float[,] psf, float lambda, float alpha, Random random, int blockSize, int threadCount, int maxIteration = 100, double epsilon = 1e-4)
+        public bool DeconvolveRandom(float[,] xImage, float[,] residuals, float[,] psf, float lambda, float alpha, Random random, int blockSize, int threadCount, int maxIteration = 100, double epsilon = 1e-4)
         {
-            var xImageExplore = xImage;
-            var xImageCorrection = new float[xImage.GetLength(0), xImage.GetLength(1)];
+            var xExplore = xImage;
+            var xCorrection = new float[xImage.GetLength(0), xImage.GetLength(1)];
 
             var PSFCorr = PSF.CalcPaddedFourierCorrelation(psf, new Rectangle(0, 0, residuals.GetLength(0), residuals.GetLength(1)));
-            var bMapExplore = Residuals.CalcBMap(residuals, PSFCorr, new Rectangle(0, 0, psf.GetLength(0), psf.GetLength(1)));
-            var bMapCorrection = new float[residuals.GetLength(0), residuals.GetLength(1)];
+
+            //calculate gradients for each pixel
+            var gExplore = Residuals.CalcBMap(residuals, PSFCorr, new Rectangle(0, 0, psf.GetLength(0), psf.GetLength(1)));
+            var gCorrection = new float[residuals.GetLength(0), residuals.GetLength(1)];
 
             var psf2 = PSF.CalcPSFSquared(psf);
 
-            var yBlockSize = blockSize;
-            var xBlockSize = blockSize;
-
-            var tau = 1; //theta, also number of processors.
-            var degreeOfSep = CountNonZero(psf);
+            yBlockSize = blockSize;
+            xBlockSize = blockSize;
+            degreeOfSeperability = CountNonZero(psf);
+            tau = 1; //number of processors.
+            
             var blockCount = xImage.Length / (yBlockSize * xBlockSize);
-            var beta = 1.0 + (degreeOfSep - 1.0) * (tau - 1.0) / (Math.Max(1.0, (blockCount - 1))); //arises from E.S.O of theta-nice sampling. Look at the original PCDM Paper for the explanation
-            //Theta-nice sampling: take theta number of random pixels 
+            var beta = CalcESO(tau, degreeOfSeperability, blockCount);
 
             var lipschitz = (float)PSF.CalcMaxLipschitz(psf) * yBlockSize * xBlockSize;
             lipschitz *= (float)beta;
             var theta = tau / (float)blockCount;
             var theta0 = theta;
-            var eta = 1.0 / blockCount;
+            float eta = 1.0f / blockCount;
 
             var testRestart = 0.0;
             var iter = 0;
@@ -60,23 +66,12 @@ namespace Single_Reference.Deconvolution.ToyImplementations
                     for (int y = yBlock * yBlockSize; y < (yBlock * yBlockSize + yBlockSize); y++)
                         for (int x = xBlock * xBlockSize; x < (xBlock * xBlockSize + xBlockSize); x++)
                         {
-                            var update = theta2 * bMapCorrection[y, x] + bMapExplore[y, x] + xImageExplore[y, x] * stepSize;
-                            update = ElasticNet.ProximalOperator(update, stepSize, lambda, alpha) - xImageExplore[y, x];
+                            var update = theta2 * gCorrection[y, x] + gExplore[y, x] + xExplore[y, x] * stepSize;
+                            update = ElasticNet.ProximalOperator(update, stepSize, lambda, alpha) - xExplore[y, x];
                             blocks[i, y, x] = update;
                             if (update != 0.0)
                                 containsNonZero[i] = true;
                         }
-
-                    /*
-                    RandomBlockCD2.AddInto(xImageExplore, xEUpdate, yBlock, xBlock, yBlockSize, xBlockSize);
-                    RandomBlockCD2.AddInto(bEUpdate, xEUpdate, yBlock, xBlock, yBlockSize, xBlockSize);
-
-                    var xCUpdate = -(1.0 - theta / theta0) / (theta * theta) * xEUpdate;
-                    RandomBlockCD2.AddInto(xImageCorrection, xCUpdate, yBlock, xBlock, yBlockSize, xBlockSize);
-                    RandomBlockCD2.AddInto(bCUpdate, xCUpdate, yBlock, xBlock, yBlockSize, xBlockSize);*/
-
-
-                    
                 }
 
                 //update bMaps
@@ -86,30 +81,38 @@ namespace Single_Reference.Deconvolution.ToyImplementations
                     {
                         var yBlock = blockSamples[i] / (xImage.GetLength(1) / xBlockSize);
                         var xBlock = blockSamples[i] % (xImage.GetLength(1) / xBlockSize);
-                        UpdateBMaps(i, blocks, yBlock, xBlock, psf2, bMapExplore, bMapCorrection, correctionFactor);
+                        UpdateBMaps(i, blocks, yBlock, xBlock, psf2, gExplore, gCorrection, correctionFactor);
 
                         //update reconstructed image
                         var yOffset = yBlock * yBlockSize;
                         var xOffset = xBlock * xBlockSize;
-                        for(int y = 0; y < xImageExplore.GetLength(0); y++)
-                            for(int x = 0; x < xImageExplore.GetLength(1);x++)
+                        for(int y = 0; y < xExplore.GetLength(0); y++)
+                            for(int x = 0; x < xExplore.GetLength(1);x++)
                             {
                                 var update = blocks[i, y, x];
-                                var oldExplore = xImageExplore[yOffset + y, xOffset + x];
-                                var oldCorrection = xImageCorrection[yOffset + y, xOffset + x];
+                                var oldExplore = xExplore[yOffset + y, xOffset + x];
+                                var oldCorrection = xCorrection[yOffset + y, xOffset + x];
                                 var newValue = oldExplore + update;
                                 testRestart = (1.0 - eta) * testRestart - eta * (update) * (newValue - (theta * theta * oldCorrection + oldExplore));
 
-                                xImageExplore[yOffset + y, xOffset + x] += update;
-                                xImageCorrection[yOffset + y, xOffset + x] += update * correctionFactor;
+                                xExplore[yOffset + y, xOffset + x] += update;
+                                xCorrection[yOffset + y, xOffset + x] += update * correctionFactor;
                             }
-                        
                     }
 
                 if (testRestart > 0)
                 {
-                    Console.Write("hello");
-                    break;
+                    //restart acceleration
+                    var tmpTheta = theta < 1.0f ? ((theta * theta) / (1.0f - theta)) : theta0;
+                    for (int y = 0; y < xExplore.GetLength(0); y++)
+                        for (int x = 0; x < xExplore.GetLength(1); x++)
+                        {
+                            xExplore[y, x] +=  tmpTheta* xCorrection[y, x];
+                            xCorrection[y, x] = 0;
+                            gExplore[y, x] += tmpTheta * gCorrection[y, x];
+                            gCorrection[y, x] = 0;
+                        }
+                    theta = theta0;
                 }
 
                 theta = (float)(Math.Sqrt((theta * theta * theta * theta) + 4 * (theta * theta)) - theta * theta) / 2.0f;
@@ -119,7 +122,162 @@ namespace Single_Reference.Deconvolution.ToyImplementations
             return false;
         }
 
-        public static void UpdateBMaps(int blockId, float[,,] blocks, int yB, int xB, float[,] psf2, float[,] bE, float[,] bC, float correctionFactor)
+        public bool DeconvolveActiveSet(float[,] xImage, float[,] residuals, float[,] psf, float lambda, float alpha, Random random, int blockSize, int threadCount, int maxIteration = 100, double epsilon = 1e-4)
+        {
+            var xExplore = xImage;
+            var xCorrection = new float[xImage.GetLength(0), xImage.GetLength(1)];
+
+            var PSFCorr = PSF.CalcPaddedFourierCorrelation(psf, new Rectangle(0, 0, residuals.GetLength(0), residuals.GetLength(1)));
+
+            //calculate gradients for each pixel
+            var gExplore = Residuals.CalcBMap(residuals, PSFCorr, new Rectangle(0, 0, psf.GetLength(0), psf.GetLength(1)));
+            var gCorrection = new float[residuals.GetLength(0), residuals.GetLength(1)];
+
+            var psf2 = PSF.CalcPSFSquared(psf);
+
+            yBlockSize = blockSize;
+            xBlockSize = blockSize;
+            degreeOfSeperability = CountNonZero(psf);
+            tau = 1; //number of processors
+
+            return false;
+        }
+
+        public bool DeconvolveRandomActiveSet(float[,] xExplore, float[,] xCorrection, float[,]gExplore, float[,] gCorrection, float[,] psf2, ref List<Tuple<int,int>> activeSet, int yBlockSize, int xBlockSize, float maxLipschitz, float lambda, float alpha, Random random, int threadCount, int maxIteration = 100, double epsilon = 1e-4)
+        {
+            var blockCount = activeSet.Count;
+            var beta = CalcESO(tau, degreeOfSeperability, blockCount);
+
+            var lipschitz = maxLipschitz * yBlockSize * xBlockSize;
+            lipschitz *= (float)beta;
+            var theta = tau / (float)blockCount;
+            var theta0 = theta;
+            float eta = 1.0f / blockCount;
+
+            var testRestart = 0.0;
+            var iter = 0;
+            var blocks = new float[tau, yBlockSize, xBlockSize];
+            var containsNonZero = new bool[tau];
+            var converged = false;
+            while (iter < maxIteration & !converged)
+            {
+                var xDiffMax = 0.0f;
+                for(int inner= 0; inner < (blockCount/tau); inner++)
+                {
+                    var stepSize = lipschitz * theta / theta0;
+                    var theta2 = theta * theta;
+                    var blockSamples = RandomCD.CreateSamples(blockCount, tau, random);
+
+                    //minimize blocks
+                    for (int i = 0; i < blockSamples.Length; i++)
+                    {
+                        var yBlock = blockSamples[i] / (xExplore.GetLength(1) / xBlockSize);
+                        var xBlock = blockSamples[i] % (xExplore.GetLength(1) / xBlockSize);
+
+                        containsNonZero[i] = false;
+                        for (int y = yBlock * yBlockSize; y < (yBlock * yBlockSize + yBlockSize); y++)
+                            for (int x = xBlock * xBlockSize; x < (xBlock * xBlockSize + xBlockSize); x++)
+                            {
+                                var update = theta2 * gCorrection[y, x] + gExplore[y, x] + xExplore[y, x] * stepSize;
+                                update = ElasticNet.ProximalOperator(update, stepSize, lambda, alpha) - xExplore[y, x];
+                                blocks[i, y, x] = update;
+                                if (update != 0.0)
+                                    containsNonZero[i] = true;
+                            }
+                    }
+
+                    //update bMaps
+                    var correctionFactor = -(1.0f - theta / theta0) / (theta * theta);
+                    for (int i = 0; i < blockSamples.Length; i++)
+                        if (containsNonZero[i])
+                        {
+                            var yBlock = blockSamples[i] / (xExplore.GetLength(1) / xBlockSize);
+                            var xBlock = blockSamples[i] % (xExplore.GetLength(1) / xBlockSize);
+                            UpdateBMaps(i, blocks, yBlock, xBlock, psf2, gExplore, gCorrection, correctionFactor);
+
+                            var currentDiff = 0.0f;
+                            //update reconstructed image
+                            var yOffset = yBlock * yBlockSize;
+                            var xOffset = xBlock * xBlockSize;
+                            for (int y = 0; y < xExplore.GetLength(0); y++)
+                                for (int x = 0; x < xExplore.GetLength(1); x++)
+                                {
+                                    var update = blocks[i, y, x];
+                                    var oldExplore = xExplore[yOffset + y, xOffset + x];
+                                    var oldCorrection = xCorrection[yOffset + y, xOffset + x];
+                                    var newValue = oldExplore + update;
+                                    testRestart = (1.0 - eta) * testRestart - eta * (update) * (newValue - (theta * theta * oldCorrection + oldExplore));
+
+                                    currentDiff += Math.Abs(update);
+
+                                    xExplore[yOffset + y, xOffset + x] += update;
+                                    xCorrection[yOffset + y, xOffset + x] += update * correctionFactor;
+                                }
+                            xDiffMax = Math.Max(xDiffMax, currentDiff);
+                        }
+
+                    theta = (float)(Math.Sqrt((theta * theta * theta * theta) + 4 * (theta * theta)) - theta * theta) / 2.0f;
+                }
+                
+                if (testRestart > 0)
+                {
+                    //restart acceleration
+                    var tmpTheta = theta < 1.0f ? ((theta * theta) / (1.0f - theta)) : theta0;
+                    for (int y = 0; y < xExplore.GetLength(0); y++)
+                        for (int x = 0; x < xExplore.GetLength(1); x++)
+                        {
+                            xExplore[y, x] += tmpTheta * xCorrection[y, x];
+                            xCorrection[y, x] = 0;
+                            gExplore[y, x] += tmpTheta * gCorrection[y, x];
+                            gCorrection[y, x] = 0;
+                        }
+
+                    //new active set
+                    activeSet = GetActiveSet(xExplore, gExplore, lambda, alpha, maxLipschitz);
+                    blockCount = activeSet.Count;
+                    theta = tau / (float)blockCount;
+                    theta0 = theta;
+                }
+
+                if(xDiffMax < epsilon)
+                {
+                    converged = true;
+                }
+
+                iter++;
+            }
+
+            return converged;
+        }
+
+
+
+        private List<Tuple<int, int>> GetActiveSet(float[,] xExplore, float[,] gExplore, float lambda, float alpha, float lipschitz)
+        {
+            var output = new List<Tuple<int, int>>();
+            for (int i = 0; i < xExplore.GetLength(0)/yBlockSize; i++)
+                for (int j = 0; j < xExplore.GetLength(1)/xBlockSize; i++)
+                {
+                    var yPixel = i * yBlockSize;
+                    var xPixel = j * xBlockSize;
+                    var nonZero = false;
+                    for(int y = yPixel; y < yPixel+yBlockSize;y++)
+                        for(int x = xPixel; x < xPixel+xBlockSize;x++)
+                        {
+                            var tmp = gExplore[y, x] + xExplore[y, x] * lipschitz;
+                            tmp = ElasticNet.ProximalOperator(tmp, lipschitz, lambda, alpha);
+                            if (tmp != xExplore[y, x])
+                                nonZero = true;
+                        }
+
+                    if(nonZero)
+                        output.Add(new Tuple<int, int>(i, j));
+                }
+
+            return output;
+        }
+
+        public static void UpdateBMaps(int blockId, float[,,] blocks, int yB, int xB, float[,] psf2, float[,] gExplore, float[,] gCorrection, float correctionFactor)
         {
             var yPsf2Half = psf2.GetLength(0) / 2;
             var xPsf2Half = psf2.GetLength(1) / 2;
@@ -128,8 +286,8 @@ namespace Single_Reference.Deconvolution.ToyImplementations
 
             var yMin = Math.Max(yPixelIdx - yPsf2Half, 0);
             var xMin = Math.Max(xPixelIdx - xPsf2Half, 0);
-            var yMax = Math.Min(yPixelIdx + blocks.GetLength(1) - yPsf2Half + psf2.GetLength(0), bE.GetLength(0));
-            var xMax = Math.Min(xPixelIdx + blocks.GetLength(2) - xPsf2Half + psf2.GetLength(1), bE.GetLength(1));
+            var yMax = Math.Min(yPixelIdx + blocks.GetLength(1) - yPsf2Half + psf2.GetLength(0), gExplore.GetLength(0));
+            var xMax = Math.Min(xPixelIdx + blocks.GetLength(2) - xPsf2Half + psf2.GetLength(1), gExplore.GetLength(1));
             for (int globalY = yMin; globalY < yMax; globalY++)
                 for (int globalX = xMin; globalX < xMax; globalX++)
                 {
@@ -151,9 +309,29 @@ namespace Single_Reference.Deconvolution.ToyImplementations
                             correctionUpdate += update * correctionFactor; 
                         }
                     
-                    bE[globalY, globalX] += exploreUpdate;
-                    bC[globalY, globalX] += correctionUpdate;
+                    gExplore[globalY, globalX] += exploreUpdate;
+                    gCorrection[globalY, globalX] += correctionUpdate;
                 }
+        }
+
+        /// <summary>
+        /// Calculate estimated seperability overapproximation (ESO). Or: When we optimize two random pixels, how much do we expect the two values to correlate.
+        /// We use tau-nice sampling. I.E. take tau (unique) blocks uniformly at random.
+        /// </summary>
+        /// <param name="tau"></param>
+        /// <param name="degreeOfSep"></param>
+        /// <param name="blockCount"></param>
+        /// <returns></returns>
+        private static double CalcESO(int tau, int degreeOfSep, int blockCount) => 1.0 + (degreeOfSep - 1.0) * (tau - 1.0) / (Math.Max(1.0, (blockCount - 1)));
+
+        public static int[] CreateSamples(int length, int sampleCount, Random rand)
+        {
+            var samples = new HashSet<int>(sampleCount);
+            while (samples.Count < sampleCount)
+                samples.Add(rand.Next(0, length));
+            int[] output = new int[samples.Count];
+            samples.CopyTo(output);
+            return output;
         }
 
         public static void DebugCray()
@@ -174,6 +352,7 @@ namespace Single_Reference.Deconvolution.ToyImplementations
 
             FitsIO.Write(bMap, "bMapCray.fits");
         }
+
 
     }
 }
